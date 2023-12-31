b52ecd7d7aa193179001a9fd1d24091a4c16b492
ch052877 <christyanmend@gmail.com>
2023-12-30 23:59:13 -0300

    Adicionando configuracoes para application properties

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	@Autowired
+	private AppConfiguration appConfiguration;
+
+	// public DemoApplication(AppConfiguration appConfiguration) {
+	// this.appConfiguration = appConfiguration;
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -16,6 +23,11 @@ public class DemoApplication {
 	@GetMapping("/inicio")
 	public String inicio() {
 		return "Olá spring boot";
+	}
+
+	@GetMapping("/configuracao")
+	public String config() {
+		return appConfiguration.getMessage();
 	}
 
 }
