5ab656bfa1f8cd5b9f3390e25aef0e73a6fca62e
ch052877 <christyanmend@gmail.com>
2024-02-03 23:19:39 -0300

    first api in spring

--- curso/src/main/java/com/curso/curso/CursoApplication.java
+++ curso/src/main/java/com/curso/curso/CursoApplication.java
@@ -2,13 +2,21 @@ package com.curso.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CursoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
-		System.out.println("oioioi");
 	}
+
+	@GetMapping("/inicio")
+	public String inicio() {
+		return "iniciou minha primeira api web";
+	}
+
 
 }
