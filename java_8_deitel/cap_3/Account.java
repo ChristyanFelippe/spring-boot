4e94104f12f9c86bf4a98d850d0c5bb2f0565d15
Christyan Felippe Mendes <christyanmend@gmail.com>
2024-02-26 15:32:24 -0300

    Criando getters e setters para account

---
+++ java_8_deitel/cap_3/Account.java
@@ -0,0 +1,18 @@
+package java_8_deitel.cap_3;
+
+/**
+ * Account
+ */
+public class Account {
+
+    private String name;
+
+    // método para definir o nome do objeto
+    public void setName(String name) {
+        this.name = name; // armazena o nome
+    }
+
+    public String getName() {
+        return name; // retorna valor do nome para o chamador
+    }
+} // fim da classe account
